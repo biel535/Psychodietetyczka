d9cfc376ff0364221175d4f55253bf428b15c23e
tomaszjakubbiel <biel535@gmail.com>
2023-12-12 15:31:53 +0100

    1

--- src/app/dyplomy-data.ts
+++ src/app/dyplomy-data.ts
@@ -3,42 +3,42 @@ export const dyplomy = [
   {
     id: 1,
     title: 'Insulinooporność w gabinecie dietetyka',
-    image: '../assets/img-dyplomy/certyfikat - insulinooporność w gabinecie dietetyka.jpeg',
+    image: 'https://raw.githubusercontent.com/biel535/Psychodietetyczka/main/src/assets/img-dyplomy/certyfikat%20-%20insulinooporno%C5%9B%C4%87%20w%20gabinecie%20dietetyka.jpeg',
 
   },
   {
     id: 2,
     title: 'Jedz normalnie, osiągaj więcej',
-    image: '../assets/img-dyplomy/certyfikat - jedz normalnie, osiągaj więcej.jpeg',
+    image: 'https://raw.githubusercontent.com/biel535/Psychodietetyczka/main/src/assets/img-dyplomy/certyfikat%20-%20jedz%20normalnie%2C%20osi%C4%85gaj%20wi%C4%99cej.jpeg',
 
   },
   {
     id: 3,
     title: 'Konferencja dietetyki sportowej',
-    image: '../assets/img-dyplomy/certyfikat - konferencja dietetyki sportowej.jpeg',
+    image: 'https://raw.githubusercontent.com/biel535/Psychodietetyczka/main/src/assets/img-dyplomy/certyfikat%20-%20konferencja%20dietetyki%20sportowej.jpeg',
 
   },
   {
     id: 4,
     title: 'Profesjonalna obsługa klienta',
-    image: '../assets/img-dyplomy/certyfikat - profesjonalna obsługa klienta.jpeg',
+    image: 'https://raw.githubusercontent.com/biel535/Psychodietetyczka/main/src/assets/img-dyplomy/certyfikat%20-%20profesjonalna%20obs%C5%82uga%20klienta.jpeg',
   },
   {
     id: 5,
     title: 'Szkolenia Ogólnopolskiego Centrum Zaburzeń Odżywiania',
-    image: '../assets/img-dyplomy/certyfikat - szkolenia Ogólnopolskiego Centrum Zaburzeń Odżywiania.png',
+    image: 'https://raw.githubusercontent.com/biel535/Psychodietetyczka/main/src/assets/img-dyplomy/certyfikat%20-%20szkolenia%20Og%C3%B3lnopolskiego%20Centrum%20Zaburze%C5%84%20Od%C5%BCywiania.png',
 
   },
   {
     id: 6,
     title: 'Żywienie w zespole policystycznych jajników i insulinooporności',
-    image: '../assets/img-dyplomy/certyfikat - żywienie w zespole policystycznych jajników i insulinooporności.jpeg',
+    image: 'https://raw.githubusercontent.com/biel535/Psychodietetyczka/main/src/assets/img-dyplomy/certyfikat%20-%20%C5%BCywienie%20w%20zespole%20policystycznych%20jajnik%C3%B3w%20i%20insulinooporno%C5%9Bci.jpeg',
 
   },
   {
     id: 7,
     title: 'Wyrożnienie za najlepsze wyniki w nauce',
-    image: '../assets/img-dyplomy/wyrożnienie za najlepsze wyniki w nauce.jpeg',
+    image: 'https://raw.githubusercontent.com/biel535/Psychodietetyczka/main/src/assets/img-dyplomy/wyro%C5%BCnienie%20za%20najlepsze%20wyniki%20w%20nauce.jpeg',
   },
 
 ];
